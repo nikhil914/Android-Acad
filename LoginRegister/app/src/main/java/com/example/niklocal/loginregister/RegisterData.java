4d1059a486e0716215b68e84c3481bb3c5fee62f
nikhil914 <nikhilranjan914@gmail.com>
2018-07-04 03:01:44 +0530

    init

---
+++ LoginRegister/app/src/main/java/com/example/niklocal/loginregister/RegisterData.java
@@ -0,0 +1,74 @@
+package com.example.niklocal.loginregister;
+
+public class RegisterData {
+
+        int id;
+        String first_name;
+        String last_name;
+        String email_id;
+        String mobile_number;
+public String getPassword() {
+        return password;
+        }
+        public void setPassword(String password) {
+        this.password = password;
+        }
+
+        String password;
+
+public RegisterData(){
+
+        }
+
+public RegisterData(int id, String first_name, String  last_name, String email_id, String phone_number, String mobile_number){
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email_id=email_id;
+        this.mobile_number=mobile_number;
+        }
+
+
+// getting ID
+        public int getID(){
+        return this.id;
+        }
+
+// setting id
+        public void setID(int id){
+        this.id = id;
+        }
+
+public String getfirstName() {
+        // TODO Auto-generated method stub
+        return first_name;
+        }
+
+        // setting  first name
+        public void setfirstName(String first_name){
+        this.first_name =first_name;
+        }
+public String getlastName() {
+        // TODO Auto-generated method stub
+        return last_name;
+        }
+
+        public void setlastName(String last_name){
+        this.last_name =last_name;
+        }
+public String getEmailId() {
+        // TODO Auto-generated method stub
+        return email_id;
+        }
+        public void setEmailId(String email_id){
+        this.email_id =email_id;
+        }
+public String getMobNo() {
+        // TODO Auto-generated method stub
+        return mobile_number;
+        }
+
+        public void setMobNo(String mobile_number){
+        this.mobile_number=mobile_number;
+        }
+        }
